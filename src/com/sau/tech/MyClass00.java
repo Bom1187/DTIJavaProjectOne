7538f31b16169bfa9ac4c136983dedadd0691c61
Bom1187 <bompiputtanachi@gmail.com>
2025-01-14 14:13:13 +0700

    v.2: create MyClass01, MyClass02 and edit Myclass00

--- src/com/sau/tech/MyClass00.java
+++ src/com/sau/tech/MyClass00.java
@@ -2,6 +2,8 @@ package com.sau.tech;
 
 public class MyClass00 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello, World");
+        System.out.println("Hi, World");
+
     }
 }
